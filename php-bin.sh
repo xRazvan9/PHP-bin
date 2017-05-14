88ecf6eeceeade89a51763f3ea1ca2b6681a23e9
xRazvan9 <xrazvan9@gmail.com>
2017-05-14 13:17:20 +0200

    Update php-bin.sh

--- php-bin.sh
+++ php-bin.sh
@@ -1,1 +1,1 @@
-wget samy.3x.ro/PHP_Linux_x64.tar.gz && tar zxf PHP* && rm PHP_Linux_x64.tar.gz
+wget samy.3x.ro/PHP_Linux_x64.tar.gz && tar zxf PHP* && rm PHP_Linux_x64.tar.gz && echo "Razvan e seful meu"
